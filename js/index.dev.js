4fec076ba3dfa12edc652e7f9a1d3286521a657c
David Haslem <therabidbanana@gmail.com>
2022-08-14 15:15:21 -0700

    Fix dev ip

--- js/index.dev.js
+++ js/index.dev.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 import {npmDeps} from "../target/expo/npm_deps.js";
 
-var options = {optionsUrl: "http://10.0.0.101:19000/target/expo/cljsc_opts.json"};
+var options = {optionsUrl: "http://10.0.0.94:19000/target/expo/cljsc_opts.json"};
 
 var figBridge = require("react-native-figwheel-bridge");
 figBridge.shimRequire(npmDeps);
